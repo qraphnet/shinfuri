f210e623c1c2ddf2b83c1b6927a95b1954f0c604
misogohan <soypasterice@gmail.com>
2024-07-05 19:06:09 +0900

    add some validator

--- src/course.ts
+++ src/course.ts
@@ -6,9 +6,19 @@ export interface Course {
   term: Term; // 開講セメスター/ターム
   credit: Credit; // 認定単位数
 };
+export const isCourse = (value: unknown): value is Course => {
+  return 'object' === typeof value && value != null
+    && 'year' in value && 'number' === typeof value.year
+    && 'term' in value && isTerm(value.term)
+    && 'credit' in value && isCredit(value.credit);
+};
 
-export type Credit = 1 | 2; // 事実上1単位か2単位の科目しか存在しない
-export type Term = 'S' | 'S1' | 'S2' | 'A' | 'A1' | 'A2' | 'W';
+export const creditList = [1, 2] as const;
+export type Credit = (typeof creditList)[number]; // 事実上1単位か2単位の科目しか存在しない
+export const isCredit = (value: unknown): value is Credit => creditList.includes(value as any);
+export const termList = ['S', 'S1', 'S2', 'A', 'A1', 'A2', 'W'] as const;
+export const isTerm = (value: unknown): value is Term => termList.includes(value as any);
+export type Term = (typeof termList)[number];
 
 export const termToInt = (term: Term): number => ({ S: 0, S1: 0, S2: 1, A: 2, A1: 2, A2: 3, W: 4 })[term];
 
