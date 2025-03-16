da4f2dfad7f0b914ff9240850f82dc4ac8638c58
qraphnet <main@qraph.net>
2025-03-16 23:11:26 +0900

    fix: the process of Repetition.

--- src/repetition.ts
+++ src/repetition.ts
@@ -14,9 +14,12 @@ export const isRepetition = (value: unknown): value is Repetition => {
 
 export const disableAbsentBeforeRepetition: (repetition: Repetition) => (report: SpecificReport) => boolean = ({ kind, year }) =>
   report => {
+    if (report.grade != '欠席') return true;
     if ('year' in report.course) {
       if (report.course.year > year) return true;
       else if (report.course.year == year) return !(kind == '留年' || ['S', 'S1', 'S2'].includes(report.course.term));
       else return false;
-    } else return true;
+    } else {
+      return true;
+    }
   }
